6e87fb208549ad144d353db47c05c6f529593bd3
Anastasia Sh <anastasia.shulman@hm.edu>
2022-11-27 20:02:54 +0100

    frontend components

---
+++ Frontend/src/Components/SearchBar.js
@@ -0,0 +1,10 @@
+//import React from "react";
+
+//export default function SearchBar() {
+//    return (
+      // … 
+//    );
+//  }
+  
+
+//  https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_components
