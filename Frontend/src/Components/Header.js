0f4d8298e406407d9c805c054f6ae6844f60f088
Anastasia Shulman <shulman@hm.edu>
2022-12-13 20:30:26 +0100

    Added Header.css and responsive hamburger menu

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -1,13 +1,55 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+import { CSSTransition } from "react-transition-group";
+import Hamburger from 'hamburger-react'
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World, this is React!</h1>
-        <p>You are looking at a Header component.</p>
-      </div>
-    )
+export default function Header() {
+  
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
+  return (
+    <header className="Header">
+      <img src={require("../assets/logo.png")} className="Logo" alt="logo" />
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <nav className="Nav">
+        <a href="/">Home</a>
+          <a href="/">Popular</a>
+          <a href="/">New</a>
+          <a href="/">Actors</a>
+          <button>Sign in</button>
+        </nav>
+      </CSSTransition>
+      <button onClick={toggleNav} className="Burger">
+      <Hamburger color="#ffca3c" />
+      </button>
+    </header>
+  );
   }
-}
-export default Header
